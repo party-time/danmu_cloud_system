73269e0b423366f7f0315e9b9c61f812167ca0b4
liuwei@party-time.cn <jushidai123>
2017-09-13 11:20:31 +0800

    deploy#发布

--- danmu_bms_api/src/main/java/cn/partytime/controller/AddressController.java
+++ danmu_bms_api/src/main/java/cn/partytime/controller/AddressController.java
@@ -37,6 +37,7 @@ public class AddressController {
 
     @RequestMapping(value = "/page", method = RequestMethod.GET)
     public PageResultModel addressPage(Integer pageNumber , Integer pageSize ){
+
         pageNumber = pageNumber-1;
         return  danmuAddressService.findAll(pageNumber,pageSize);
     }
