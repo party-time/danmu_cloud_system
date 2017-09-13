04d53d6c920630e78553dcf358dcbbd6b87cfdfd
liuwei@party-time.cn <jushidai123>
2017-09-13 11:12:45 +0800

    deploy#发布

--- danmu_clientSever_api/src/main/java/cn/partytime/controller/FlashClientController.java
+++ danmu_clientSever_api/src/main/java/cn/partytime/controller/FlashClientController.java
@@ -23,7 +23,6 @@ public class FlashClientController {
 
     @RequestMapping(value = "/config", method = RequestMethod.GET)
     public JSONObject config(String code){
-
         JSONObject jsonObject = null;
         try{
             jsonObject = flashConfigService.createConfig(code);
