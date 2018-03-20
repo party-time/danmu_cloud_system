49bb8a630e6455d6e1e7e1d6dd4271ccb8b0530e
soaryang <759620299@qq.com>
2018-03-20 16:42:34 +0800

    deploy#定时任务报错

--- danmu_bms_api/src/main/java/cn/partytime/controller/TaskController.java
+++ danmu_bms_api/src/main/java/cn/partytime/controller/TaskController.java
@@ -2,6 +2,7 @@ package cn.partytime.controller;
 
 import cn.partytime.model.RestResultModel;
 import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by administrator on 2017/12/18.
  */
+
+@Slf4j
 @RestController
 @RequestMapping(value = "/v1/api/admin/task")
 public class TaskController {
@@ -22,7 +25,7 @@ public class TaskController {
     @RequestMapping(value = "/taskController", method = RequestMethod.GET)
     public RestResultModel del(String taskName) {
         RestResultModel restResultModel =new RestResultModel();
-        System.out.println("taskName--------------------:"+taskName);
+        log.info("taskName--------------------:"+taskName);
 
         redisTemplate.convertAndSend("secheduler:command:key", taskName);
 
