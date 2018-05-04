0131012b6f2d3e2879d35bd848f463698a3b95e0
soaryang <759620299@qq.com>
2018-05-04 14:24:38 +0800

    deploy#获取历史弹幕

--- danmu_bms_api/src/main/java/cn/partytime/controller/wechat/WechatMiniRestController.java
+++ danmu_bms_api/src/main/java/cn/partytime/controller/wechat/WechatMiniRestController.java
@@ -237,7 +237,7 @@ public class WechatMiniRestController {
     public PageResultModel historyDanmu(@PathVariable("openId")String openId , @PathVariable("pageNo") Integer pageNo, @PathVariable("pageSize")Integer pageSize) {
         PartyLogicModel party = bmsWechatUserService.findPartyByOpenId(openId);
         log.info("party:{}",JSON.toJSONString(party));
-        PageResultModel pageResultModel = bmsDanmuService.findPageResultModel(pageNo,pageSize,party.getAddressId(),party.getPartyId(),1);
+        PageResultModel pageResultModel = bmsDanmuService.findPageResultModel(pageNo-1,pageSize,party.getAddressId(),party.getPartyId(),1);
         return pageResultModel;
     }
 }
