b6dd074622f569c3423ec8faa68511ab57490784
soaryang <759620299@qq.com>
2018-05-10 16:03:47 +0800

    爬虫不好用

--- danmu_cloud_scheduler/src/main/java/cn/partytime/SchedulerApplication.java
+++ danmu_cloud_scheduler/src/main/java/cn/partytime/SchedulerApplication.java
@@ -12,10 +12,10 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @RefreshScope
-public class Application {
+public class SchedulerApplication {
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(SchedulerApplication.class, args);
 	}
 
 }
