db3fc75af4d886ecef0eb248696c0855e592cb91
soaryang <759620299@qq.com>
2018-07-04 18:52:53 +0800

    审核节目显示历史弹幕

--- danmu_data_service/src/main/java/cn/partytime/service/DanmuService.java
+++ danmu_data_service/src/main/java/cn/partytime/service/DanmuService.java
@@ -132,9 +132,9 @@ public class DanmuService {
         Criteria criteria = new Criteria().andOperator(
                 Criteria.where("danmuSrc").is(danmuSrc),
                 Criteria.where("isBlocked").is(isBlocked),
-                Criteria.where("viewFlg").is(viewFlg),
+                Criteria.where("viewFlg").is(viewFlg)/*,
                 Criteria.where("updateTime").gte(startDate),
-                Criteria.where("updateTime").lte(endDate)
+                Criteria.where("updateTime").lte(endDate)*/
         );
 
         Map<String, Object> result = new HashMap<>();
