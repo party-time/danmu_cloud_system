d53cdc2e1f9854f76802ecc46a2a97f89ba04462
soaryang <759620299@qq.com>
2018-07-23 17:29:54 +0800

    commit

--- danmu_cloud_scheduler/src/main/java/cn/partytime/scheduler/impl/UserCountScheduler.java
+++ danmu_cloud_scheduler/src/main/java/cn/partytime/scheduler/impl/UserCountScheduler.java
@@ -4,6 +4,7 @@ package cn.partytime.scheduler.impl;
 import cn.partytime.cache.user.WechatUserCountCacheService;
 import cn.partytime.dataRpc.RpcDanmuAddressService;
 import cn.partytime.dataRpc.RpcWechatService;
+import cn.partytime.scheduler.BaseScheduler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -11,17 +12,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service("uerCountScheduler")
 @EnableScheduling
 @RefreshScope
 @Slf4j
-public class UserCountScheduler {
+public class UserCountScheduler  implements BaseScheduler {
 
     @Autowired
     private RpcWechatService rpcWechatService;
 
     @Scheduled(cron = "0 26 17 ? * MON")
-    public void countUser(){
+    public void execute() throws IOException {
         log.info("执行统计用户数据定时任务");
         rpcWechatService.countNewWechatUser();
     }
