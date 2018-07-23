f8dd3323a77a324da20a0f35c13848133ea5b2b0
soaryang <759620299@qq.com>
2018-07-23 17:39:54 +0800

    commit

--- danmu_cloud_scheduler/src/main/java/cn/partytime/scheduler/impl/UserCountScheduler.java
+++ danmu_cloud_scheduler/src/main/java/cn/partytime/scheduler/impl/UserCountScheduler.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-@Service("uerCountScheduler")
+@Service("userCountScheduler")
 @EnableScheduling
 @RefreshScope
 @Slf4j
