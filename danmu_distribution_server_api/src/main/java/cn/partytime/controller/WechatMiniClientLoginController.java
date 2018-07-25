6d0c51ba3626f65d6a1e8fc961b9dd1ae454d36f
soaryang <759620299@qq.com>
2018-07-25 16:24:23 +0800

    commit

--- danmu_distribution_server_api/src/main/java/cn/partytime/controller/WechatMiniClientLoginController.java
+++ danmu_distribution_server_api/src/main/java/cn/partytime/controller/WechatMiniClientLoginController.java
@@ -5,6 +5,7 @@ import cn.partytime.cache.wechatmin.WechatMiniCacheService;
 import cn.partytime.logicService.WechatMiniClientLoginService;
 import cn.partytime.model.ResultInfo;
 import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/distribute/wechatMini")
 public class WechatMiniClientLoginController {
@@ -24,7 +26,7 @@ public class WechatMiniClientLoginController {
 
     @RequestMapping(value = "/login/{userKey}" ,method = RequestMethod.GET)
     public ResultInfo view(HttpServletRequest request, @PathVariable("userKey") String userKey) {
-
+        log.info("----------------微信小程序登录------------{}",userKey);
         return wechatMiniClientLoginService.findCollectorInfo(userKey);
     }
 
