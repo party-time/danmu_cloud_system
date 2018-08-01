e8351427dfd206b84bcae5cc56f7e38e1afc2dd9
liuwei@party-time.cn <jushidai123>
2018-08-01 17:19:20 +0800

    #发布  test

--- danmu_bms_api/src/main/java/cn/partytime/filter/AuthInterceptor.java
+++ danmu_bms_api/src/main/java/cn/partytime/filter/AuthInterceptor.java
@@ -104,6 +104,7 @@ public class AuthInterceptor implements HandlerInterceptor {
                         return false;
                     }
                 }
+
                 if(!StringUtils.isEmpty(adminUser.getRoleId()) && adminUser.getRoleId().equals("5b6166ade6e9b84788fcbc5f")){
                     if( url.indexOf("/order") ==-1 && !url.equals("/v1/logout") ){
                         response.setStatus(466);
