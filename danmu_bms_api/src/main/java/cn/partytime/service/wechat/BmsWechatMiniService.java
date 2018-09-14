2c2bce8f2df4ccfde28d9052819092ace278e735
liuwei@party-time.cn <jushidai123>
2018-09-14 16:00:52 +0800

    修复bug设备列表

--- danmu_bms_api/src/main/java/cn/partytime/service/wechat/BmsWechatMiniService.java
+++ danmu_bms_api/src/main/java/cn/partytime/service/wechat/BmsWechatMiniService.java
@@ -1,10 +1,8 @@
 package cn.partytime.service.wechat;
 
 
-import com.alibaba.fastjson.JSON;
-import com.baidu.aip.speech.AipSpeech;
+
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,11 +30,11 @@ public class BmsWechatMiniService {
         log.info("appKey:{}",appKey);
         log.info("secret:{}",secret);
         // 初始化一个AipSpeech
-        AipSpeech client = new AipSpeech(appId, appKey, secret);
+        //AipSpeech client = new AipSpeech(appId, appKey, secret);
 
         // 可选：设置网络连接参数
-        client.setConnectionTimeoutInMillis(2000);
-        client.setSocketTimeoutInMillis(60000);
+        //client.setConnectionTimeoutInMillis(2000);
+       // client.setSocketTimeoutInMillis(60000);
 
         // 可选：设置代理服务器地址, http和socket二选一，或者均不设置
         //client.setHttpProxy("proxy_host", proxy_port);  // 设置http代理
@@ -46,20 +44,22 @@ public class BmsWechatMiniService {
         // 也可以直接通过jvm启动参数设置此环境变量
         //System.setProperty("aip.log4j.conf", "path/to/your/log4j.properties");
         // 调用接口
-        JSONObject res = client.asr(path, "pcm", 16000, null);
+        //JSONObject res = client.asr(path, "pcm", 16000, null);
         //JSONArray jsonArray = res.getJSONArray("result");
         //log.info("jsonArray:{}",JSON.toJSONString(jsonArray));
         //System.out.println(res.toString(2));
         //log.info("res.toString()-----------------"+res.toString(2));
         //return res.toString();
 
-        Object object = res.get("result");
+        //Object object = res.get("result");
 
         StringBuffer stringBuffer = new StringBuffer();
-        com.alibaba.fastjson.JSONArray jsonArray = com.alibaba.fastjson.JSONObject.parseArray(String.valueOf(object));
+        //com.alibaba.fastjson.JSONArray jsonArray = com.alibaba.fastjson.JSONObject.parseArray(String.valueOf(object));
+        /*
         for(int i=0; i<jsonArray.size(); i++){
             stringBuffer.append(String.valueOf(jsonArray.get(i)).replace("，",""));
         }
+        */
         return stringBuffer.toString();
     }
 
